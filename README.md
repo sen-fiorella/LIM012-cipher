f70a0b81f68dabb5ec256390979044784ccfa928
sen-fiorella <sendy.fb@gmail.com>
2020-02-18 11:50:33 -0500

    cifrado mayuscula, minuscula y espaciado

--- README.md
+++ README.md
@@ -1,20 +1,22 @@
-<!-- # El secreto del César -->
+<!-- # Diario personal virtual-->
 
-
-
-![]()
-![](./assets/imagenes/prototipo.jpg)
-
-
-Debe contener lo siguiente:
-
-Un título con el nombre de tu proyecto.
-Un resumen de 1 o 2 líneas de qué se trata tu proyecto.
+Título: Diario personal virtual: My secret diary
+Resumen:
+¿De qué se trata?
+    Plataforma virtual que permite a los usuarios cifrar y descifrar el contenido de su diario privado. 
 La imagen final de tu proyecto.
+
 Investigación UX:
+
 Explicar quiénes son los usuarios y los objetivos en relación con el producto.
-Explicar cómo el producto soluciona los problemas/necesidades de dichos usuarios.
+    La aplicación está destinada especialmente a usuarios de 15 a 35 años.
+    Con esta aplicación el usuario puede cifrar y descifrar el contenido de su diario personal o cualquier texto en general.
+    Mediante el método del Cifrado César, la aplicación resuelve los problemas de confidencialidad y seguridad a los cuales se exponen los usuarios al guardar información digital en sus archivos personales.
+    El cifrado y el descifrado de los textos se realizan de forma sencilla y rápida.
+
 Luego colocarás la foto de tu primer prototipo en papel.
+
 Agregar un resumen del feedback recibido indicando las mejoras a realizar.
+
 Imagen del prototipo final.
 
--- src/index.js
+++ src/index.js
@@ -20,36 +20,53 @@ buttonCode.addEventListener('click',() =>
     for (let i = 0; i < message.length; i++) {
       //Traigo la posición ascii de la letra ingresada en message - Coloco 0 para indicar la posición inicial del string, aquí aplicar for
     let codeAscii= message.charCodeAt(i);
-    //Aplico la fórmula de Cifrado César para el desplazamiento
-    let cifrado = (codeAscii-65+parseInt(valueOffset))%26+65;
-    textResultado.value += String.fromCharCode(cifrado);
-    }
+    if(codeAscii >= 65 && codeAscii <= 90) {
+      //Aplico la fórmula de Cifrado César para el desplazamiento
+      let cifradoMayus = (codeAscii-65+parseInt(valueOffset))%26+65;
+      textResultado.value += String.fromCharCode(cifradoMayus);
 
-    
-
+      } else if(codeAscii >= 97 && codeAscii <= 122) {
+        //Aplico la fórmula de Cifrado César para el desplazamiento
+        let cifradoMinus = (codeAscii-97+parseInt(valueOffset))%26+97;
+        textResultado.value += String.fromCharCode(cifradoMinus);
+      } else if(codeAscii === 32)
+      
+      {
+      textResultado.value += ' ';
+      }
+      
+  }
     
    
   });
-
 //Creando evento al boton Descifrar
 buttonDecode.addEventListener('click', () => 
   {
     let valueOffset = offset.value;
     let message = textIngreso.value;
 
-  //For
-  for (let i = 0; i < message.length; i++) {
-    //Traigo la posición ascii de la letra ingresada en message - Coloco 0 para indicar la posición inicial del string, aquí aplicar for
-  let codeAscii= message.charCodeAt(i);
-  let textDescifrado = textResultado.value;
-  //Aplico la fórmula de Cifrado César para el desplazamiento
-  let descifrado = (codeAscii+65-parseInt(valueOffset))%26+65;
-  textResultado.value += String.fromCharCode(descifrado);
-
+    //For
+    for (let i = 0; i < message.length; i++) {
+      //Traigo la posición ascii de la letra ingresada en message - Coloco 0 para indicar la posición inicial del string, aquí aplicar for
+    let codeAscii= message.charCodeAt(i);
+    if (codeAscii >= 65 && codeAscii <= 90) {
+      // let textDescifrado = textResultado.value; (comentado para probar, luego sacar)
+      //Aplico la fórmula de Cifrado César para el desplazamiento
+      let descifradoMayus = (codeAscii-90-parseInt(valueOffset))%26+90;
+      textResultado.value += String.fromCharCode(descifradoMayus);
+      }
+      else if(codeAscii >= 97 && codeAscii <= 122) {
+      //Aplico la fórmula de Cifrado César para el desplazamiento
+      let cifradoMinus = (codeAscii-122-parseInt(valueOffset))%26+122;
+      textResultado.value += String.fromCharCode(cifradoMinus);
+      }
+      else if (codeAscii === 32)
+    {
+      textResultado.value += ' ';
+    }
     
   }
-
-
-});
+  
+  });
 
 
