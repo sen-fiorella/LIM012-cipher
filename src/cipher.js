6e90e77a458991a2076d39b23f7c5217feebbecd
sen-fiorella <sendy.fb@gmail.com>
2020-02-20 09:52:49 -0500

    modularizar

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,52 @@
 const cipher = {
+  encode: (message,valueOffset) => {
+    for (let i = 0; i < message.length; i++) {
+      //Traigo la posición ascii de la letra ingresada en message - Coloco 0 para indicar la posición inicial del string, aquí aplicar for
+    let codeAscii= message.charCodeAt(i);
+    if(codeAscii >= 65 && codeAscii <= 90) {
+      //Aplico la fórmula de Cifrado César para el desplazamiento
+      let cifradoMayus = (codeAscii-65+parseInt(valueOffset))%26+65;
+      textResultado.value += String.fromCharCode(cifradoMayus);
+      
+      } else if(codeAscii >= 97 && codeAscii <= 122) {
+        //Aplico la fórmula de Cifrado César para el desplazamiento
+        let cifradoMinus = (codeAscii-97+parseInt(valueOffset))%26+97;
+        textResultado.value += String.fromCharCode(cifradoMinus);
+      } else if(codeAscii >=32 && codeAscii<=64) {
+        let cifradoNumCar = (codeAscii-32+parseInt(valueOffset))%33+32;
+        textResultado.value += String.fromCharCode(cifradoNumCar);
+      }    
+  }
+  return textResultado.value
+},
 
-  
-  // ...
+  decode: (message,valueOffset) => {
+
+    //For
+    for (let i = 0; i < message.length; i++) {
+      //Traigo la posición ascii de la letra ingresada en message - Coloco 0 para indicar la posición inicial del string, aquí aplicar for
+    let codeAscii= message.charCodeAt(i);
+    if (codeAscii >= 65 && codeAscii <= 90) {
+      // let textDescifrado = textResultado.value; (comentado para probar, luego sacar)
+      //Aplico la fórmula de Cifrado César para el desplazamiento
+      let descifradoMayus = (codeAscii-90-parseInt(valueOffset))%26+90;
+      textResultado.value += String.fromCharCode(descifradoMayus);
+      }
+      else if(codeAscii >= 97 && codeAscii <= 122) {
+      //Aplico la fórmula de Cifrado César para el desplazamiento
+      let descifradoMinus = (codeAscii-122-parseInt(valueOffset))%26+122;
+      textResultado.value += String.fromCharCode(descifradoMinus);
+      }
+      else if (codeAscii >= 32 && codeAscii <=64) {
+      let descifradoNumCar = (codeAscii-64-parseInt(valueOffset))%33+64;
+      textResultado.value += String.fromCharCode(descifradoNumCar);
+    }
+    
+  }
+
+  return textResultado.value
+ }
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,70 +1,29 @@
-//import cipher from './cipher.js';
+// import { encode } from "punycode";
 
-//console.log(cipher);
+import cipher from './cipher.js';
 
-//Variables
+// console.log(cipher);
+
 const textIngreso = document.getElementById('textIngreso');
 const textResultado = document.getElementById('textResultado');
 const buttonCode = document.getElementById('bCode');
 const buttonDecode = document.getElementById('bDecode');
 let offset = document.getElementById("offset");
 
-
-//Creando evento al boton Cifrar
 buttonCode.addEventListener('click',() => 
   {
     let valueOffset = offset.value;
     let message = textIngreso.value;
-    
-    //for
-    for (let i = 0; i < message.length; i++) {
-      //Traigo la posición ascii de la letra ingresada en message - Coloco 0 para indicar la posición inicial del string, aquí aplicar for
-    let codeAscii= message.charCodeAt(i);
-    if(codeAscii >= 65 && codeAscii <= 90) {
-      //Aplico la fórmula de Cifrado César para el desplazamiento
-      let cifradoMayus = (codeAscii-65+parseInt(valueOffset))%26+65;
-      textResultado.value += String.fromCharCode(cifradoMayus);
-      
-      } else if(codeAscii >= 97 && codeAscii <= 122) {
-        //Aplico la fórmula de Cifrado César para el desplazamiento
-        let cifradoMinus = (codeAscii-97+parseInt(valueOffset))%26+97;
-        textResultado.value += String.fromCharCode(cifradoMinus);
-      } else if(codeAscii >=32 && codeAscii<=64) {
-        let cifradoNumCar = (codeAscii-32+parseInt(valueOffset))%33+32;
-        textResultado.value += String.fromCharCode(cifradoNumCar);
-      }    
-  }
-    
+    textResultado.value = cipher.encode(message,valueOffset);    
    
   });
+
 //Creando evento al boton Descifrar
 buttonDecode.addEventListener('click', () => 
   {
     let valueOffset = offset.value;
     let message = textIngreso.value;
-
-    //For
-    for (let i = 0; i < message.length; i++) {
-      //Traigo la posición ascii de la letra ingresada en message - Coloco 0 para indicar la posición inicial del string, aquí aplicar for
-    let codeAscii= message.charCodeAt(i);
-    if (codeAscii >= 65 && codeAscii <= 90) {
-      // let textDescifrado = textResultado.value; (comentado para probar, luego sacar)
-      //Aplico la fórmula de Cifrado César para el desplazamiento
-      let descifradoMayus = (codeAscii-90-parseInt(valueOffset))%26+90;
-      textResultado.value += String.fromCharCode(descifradoMayus);
-      }
-      else if(codeAscii >= 97 && codeAscii <= 122) {
-      //Aplico la fórmula de Cifrado César para el desplazamiento
-      let descifradoMinus = (codeAscii-122-parseInt(valueOffset))%26+122;
-      textResultado.value += String.fromCharCode(descifradoMinus);
-      }
-      else if (codeAscii >= 32 && codeAscii <=64) {
-      let descifradoNumCar = (codeAscii-64-parseInt(valueOffset))%33+64;
-      textResultado.value += String.fromCharCode(descifradoNumCar);
-    }
-    
-  }
-  
-  });
+    textResultado.value = cipher.decode(message,valueOffset);    
+});
 
 
